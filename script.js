83a24060753392fd5716557a4a34154c4623ce66
SOURADEEP HAZRA <105676331+Souradeephazra123@users.noreply.github.com>
2023-04-09 04:06:22 +0530

    Updated script.js
    refracted the whole code and corrected (.guess).textContent to (.guess).value in the 105 line to show no value in guess(class).

--- script.js
+++ script.js
@@ -31,21 +31,25 @@ let score = 20;
 let highscore = 0;
 
 // document.querySelector('.number').textContent = secretNumber;
-
+//replacing a function that replaces the large sentence
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
 //we add eventlistener and event handler
 /* after click some event happened what you set in event handler*/
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
   console.log(typeof guess);
+
   //if there are no value then write no number
   if (!guess) {
-    document.querySelector('.message').textContent = '☎️No number';
+    displayMessage('☎️No number');
   }
 
   //if the number matched then write correct number
   else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '🎉Correct Number!';
+    displayMessage('🎉Correct Number!');
     document.querySelector('.number').textContent = secretNumber;
     // change the high score value
     document.querySelector('.highscore').textContent = secretNumber;
@@ -53,36 +57,41 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = 'green';
     //when answer is correct then double the width of the answer boxs
     document.querySelector('.number').style.width = '30rem';
-
-    //when score is greater than highscore then update highscore=score
-    if (score > highscore) {
-      highscore = score;
-      document.querySelector('.highscore').textContent = highscore;
-    }
   }
-
-  //if guess>secretNumber then write too high
-  else if (guess > secretNumber) {
+  //if guess is different than secretNumber
+  else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = '📈 Too High!';
+      displayMessage(guess > secretNumber ? '📈Too High' : '📉Too low');
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = '😒 You lost the game';
+      displayMessage('😒 You lost the game');
       document.querySelector('score').textContent = 0;
     }
   }
-  //if guess<secretNumber then write too low
-  else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '📉 Too Low!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '😒 You lost the game';
-      document.querySelector('score').textContent = 0;
-    }
-  }
+
+  //   //if guess>secretNumber then write too high
+  //   else if (guess > secretNumber) {
+  //     if (score > 1) {
+  //       document.querySelector('.message').textContent = '📈 Too High!';
+  //       score--;
+  //       document.querySelector('.score').textContent = score;
+  //     } else {
+  //       document.querySelector('.message').textContent = '😒 You lost the game';
+  //       document.querySelector('score').textContent = 0;
+  //     }
+  //   }
+  //   //if guess<secretNumber then write too low
+  //   else if (guess < secretNumber) {
+  //     if (score > 1) {
+  //       document.querySelector('.message').textContent = '📉 Too Low!';
+  //       score--;
+  //       document.querySelector('.score').textContent = score;
+  //     } else {
+  //       document.querySelector('.message').textContent = '😒 You lost the game';
+  //       document.querySelector('score').textContent = 0;
+  //     }
+  //   }
 });
 
 //coding challenges #1
@@ -90,10 +99,16 @@ document.querySelector('.check').addEventListener('click', function () {
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secretNumber = Math.floor(Math.random() * 20) + 1;
-  document.querySelector('.message').textContent = 'Start Guessing...';
+  displayMessage('Start Guessing...');
   document.querySelector('.score').textContent = score;
   document.querySelector('.number').textContent = '?';
-  document.querySelector('.guess').textContent = '';
+  document.querySelector('.guess').value = '';
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
 });
+
+//when score is greater than highscore then update highscore=score
+if (score > highscore) {
+  highscore = score;
+  document.querySelector('.highscore').textContent = highscore;
+}
